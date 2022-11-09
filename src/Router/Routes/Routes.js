c8643e4e0d615415dc62dea648fde7faa46a8b92
Imam-Hossain <zunayedemon6t9@gmail.com>
2022-11-09 10:23:51 +0600

    footer added

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../main/Main";
 import Home from '../../components/Home/Home'
+import Service from '../../components/services/Services'
+import Blog from "../../components/blog/Blog";
+import Login from "../../components/login/Login";
+import Register from "../../components/register/Register";
+
 
 
 export const router = createBrowserRouter([
@@ -8,6 +13,10 @@ export const router = createBrowserRouter([
     children:[
         {path:"/", element: <Home/>},
         {path:"/home", element: <Home/>},
+        {path:"/service", element: <Service/> },
+        {path:"/blog", element: <Blog/> },
+        {path:"/login", element: <Login/> },
+        {path:"/register", element: <Register/> },
     ]
 
 }

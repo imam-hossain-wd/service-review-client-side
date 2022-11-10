0fadb9be52ca2bcb996b2710b45de373e473c148
Imam-Hossain <zunayedemon6t9@gmail.com>
2022-11-10 12:02:06 +0600

    added cart details

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -4,9 +4,9 @@ import { useLoaderData } from 'react-router-dom';
 const Checkout = () => {
     const { _id, title, price } = useLoaderData();
     return (
-        <div>
+        <div className=' mx-auto' style={{width:"700px"}}>
             <form >
-                <h2 className="text-4xl">You are about to order: {title}</h2>
+                <h2 className="text-4xl text-center">You are about to order: {title}</h2>
                 <h4 className="text-3xl">Price: {price}</h4>
                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
                     <input name="firstName" type="text" placeholder="First Name" className="input input-ghost w-full  input-bordered" />
@@ -14,9 +14,9 @@ const Checkout = () => {
                     <input name="phone" type="text" placeholder="Your Phone" className="input input-ghost w-full  input-bordered" required />
                     <input name="email" type="text" placeholder="Your email"  className="input input-ghost w-full  input-bordered" readOnly />
                 </div>
-                <textarea name="message" className="textarea textarea-bordered h-24 w-full" placeholder="Your Message" required></textarea>
+                <textarea name="message" className="textarea textarea-bordered h-24 w-full mt-3" placeholder="Your Message" required></textarea>
 
-                <input className='btn' type="submit" value="Place Your Order" />
+                <input className='btn mt-2' type="submit" value="Place Your Order" />
             </form>
         </div>
     );
--- src/components/Home/Service.js
+++ src/components/Home/Service.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Service = ({service}) => {
-    const { _id, img, price, name,title,date } = service;
+    const { _id, img, price, name,title,date, details,rating } = service;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <h2 className="card-title">{name}</h2>
+                <p>{details}</p>
+                <p>{rating}</p>
                 <p className='text-2xl text-orange-600 font-semibold'>Price: ${price}</p>
                 <p>{date}</p>
                 <div className="card-actions justify-end">
